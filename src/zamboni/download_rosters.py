82032cd400c15ed008846ca3d9ec0407446c5dbc
nluongo <nluongo@gmail.com>
2024-05-19 22:16:08 -0500

    Download rosters

--- src/zamboni/download_rosters.py
+++ src/zamboni/download_rosters.py
@@ -1,18 +1,31 @@
 import requests
+from datetime import datetime
 
-team_abbrev = 'DET'
-start_year = 2019
-end_year = 2020
-r = requests.get(f'https://api-web.nhle.com/v1/roster/{team_abbrev}/{start_year}{end_year}')
-info_json = r.json()
-print(info_json)
-forwards = info_json['forwards']
-defensemen = info_json['defensemen']
-goalies = info_json['goalies']
-players = forwards + defensemen + goalies
+with open('data/teams.txt', 'r') as f_teams:
+    team_lines = f_teams.readlines()
+    team_lines = [line.split(',') for line in team_lines]
+    team_abbrevs = [line[1].strip() for line in team_lines]
 
-for player in players:
-    first_name = player['firstName']['default']
-    last_name = player['lastName']['default']
-    print(f'{team_abbrev} {start_year} {first_name} {last_name}')
+start_year = 1900
+end_year = start_year + 1
 
+with open('data/rosterEntry.txt', 'w') as roster_f:
+    while start_year < datetime.now().year:
+        end_year = start_year + 1
+        for team in team_abbrevs:
+            try:
+                r = requests.get(f'https://api-web.nhle.com/v1/roster/{team}/{start_year}{end_year}')
+                info_json = r.json()
+            except:
+                continue
+            forwards = info_json['forwards']
+            defensemen = info_json['defensemen']
+            goalies = info_json['goalies']
+            players = forwards + defensemen + goalies
+    
+            for player in players:
+                first_name = player['firstName']['default']
+                last_name = player['lastName']['default']
+                entry_str = f'{team}, {start_year}, {first_name}, {last_name}, {start_year}, {end_year}\n'
+                roster_f.write(entry_str)
+        start_year += 1
