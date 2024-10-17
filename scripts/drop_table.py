2d0581b24a38fed85ec4fce660545d45f9cec56f
nluongo <nluongo@gmail.com>
2024-10-16 21:43:08 -0500

    Move drop_table to scripts

--- scripts/drop_table.py
+++ scripts/drop_table.py
@@ -1,9 +1,9 @@
-from zamboni.db_con import DBConnector
+from zamboni import DBConnector
 
 db = DBConnector()
 conn = db.connect_db()
 cursor = conn.cursor()
 
-sql = '''DROP TABLE teams'''
+sql = '''DROP TABLE games'''
 cursor.execute(sql)
 conn.commit()
