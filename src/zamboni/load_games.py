5821a9cffd4d2a065830c2485b4611bf738e9b25
nluongo <nluongo@gmail.com>
2024-10-16 21:52:18 -0500

    Team IDs when loading games

--- src/zamboni/load_games.py
+++ src/zamboni/load_games.py
@@ -1,25 +1,51 @@
 from zamboni.db_con import DBConnector
+from collections import defaultdict
 
 db = DBConnector()
 conn = db.connect_db()
 cursor = conn.cursor()
 
-with open('data/games.txt') as f:
+team_id_dict = defaultdict(lambda: 'Undefined')
+
+def get_team_id(id_dict, team_abbrev):
+    if id_dict[team_abbrev] == 'Undefined':
+        query_sql = f'''SELECT id FROM teams WHERE nameAbbrev="{team_abbrev}"'''
+        query_res = cursor.execute(query_sql)
+        query_out = query_res.fetchone()
+        if query_out:
+            team_id = query_out[0]
+            id_dict[team_abbrev] = team_id
+        else:
+            raise ValueError(f'No team record found with name {team_abbrev}')
+    else:
+        team_id = id_dict[team_abbrev]
+    return team_id
+    
+with open('data/games.txt', 'r') as f:
     for line in f.readlines():
         split_line = line.split(',')
         split_line = [entry.strip() for entry in split_line]
-        api_id, home_team_id, away_team_id, date_played, time_played, season_id, home_team_goals, away_team_goals, game_type, last_period_type = split_line
-        sql = f'''INSERT INTO games(apiId, homeTeamID, awayTeamID, datePlayed, timePlayed, seasonID, homeTeamGoals, awayTeamGoals, gameTypeID, lastPeriodTypeID)
+        api_id, season_id, home_api_id, home_team_abbrev, away_api_id, away_team_abbrev, date_played, time_played, home_team_goals, away_team_goals, game_type, last_period_type = split_line
+        home_team_id = get_team_id(team_id_dict, home_team_abbrev)
+        away_team_id = get_team_id(team_id_dict, away_team_abbrev)
+        if home_team_goals > away_team_goals:
+            outcome = 1
+        elif home_team_goals < away_team_goals:
+            outcome = -1
+        else:
+            outcome = 0
+        sql = f'''INSERT INTO games(apiId, seasonID, homeTeamID, awayTeamID, datePlayed, timePlayed, homeTeamGoals, awayTeamGoals, gameTypeID, lastPeriodTypeID, outcome)
                 VALUES("{api_id}", \
+                       "{season_id}", \
                        "{home_team_id}", \
                        "{away_team_id}", \
                        "{date_played}", \
                        "{time_played}", \
-                       "{season_id}", \
                        "{home_team_goals}", \
                        "{away_team_goals}", \
                        "{game_type}", \
-                       "{last_period_type}" \
+                       "{last_period_type}", \
+                       "{outcome}" \
                        )'''
         cursor.execute(sql)
         conn.commit()
